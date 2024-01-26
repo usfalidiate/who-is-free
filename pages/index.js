857811401c9f66b3bfc158e6cca0f87483c067cd
usfalidiate <51782274+usfalidiate@users.noreply.github.com>
2024-01-27 01:10:50 +1100

    beter

--- pages/index.js
+++ pages/index.js
@@ -4615,9 +4615,9 @@ function HowToUse() {
         </div>
 
         <div className='htuText'>
-          1. Click on the 'Let Us Jam' Button <br/>
-          2. Click on the 'Show Login' Button <br/>
-          3. Click on the 'Create Account' Button <br/>
+          1. Click on the Let Us Jam Button <br/>
+          2. Click on the Show Login Button <br/>
+          3. Click on the Create Account Button <br/>
           4. Enter a User Name and Password <br/>
           5. Share this User Name and Password with all members of the band <br/>
           6. Each band member uses the same User Name and Password to log into the Band Account <br/>
